3fd5783f225c70293ceebd8b39134bfa145a6c13
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2020-12-14 17:04:07 -0700

    some variables

---
+++ app/main.js
@@ -0,0 +1,39 @@
+import Anteater from './models/Anteater.js'
+import Bear from './models/Bear.js'
+import Cougar from './models/Cougar.js'
+import Donkey from './models/Donkey.js'
+import Elephant from './models/Elephant.js'
+import Fox from './models/Fox.js'
+import Giraffe from './models/Giraffe.js'
+import HoneyBadger from './models/HoneyBadger.js'
+import Iguana from './models/Iguana.js'
+import Jackrabbit from './models/Jackrabbit.js'
+import Kangaroo from './models/Kangaroo.js'
+import Lion from './models/Lion.js'
+import Monkey from './models/Monkey.js'
+import Narwhal from './models/Narwhal.js'
+import Ostrich from './models/Ostrich.js'
+import Panther from './models/Panther.js'
+import Quokka from './models/Quokka.js'
+import Rhino from './models/Rhino.js'
+import Squid from './models/Squid.js'
+import TRex from './models/TRex.js'
+import Unicorn from './models/Unicorn.js'
+import Vulture from './models/Vulture.js'
+import Whale from './models/Whale.js'
+import Xeme from './models/Xeme.js'
+import Yak from './models/Yak.js'
+import Zebra from "./models/Zebra.js"
+
+let Angela = new Anteater("Angela", "Female", 25, 13, 1200)
+let Bernard = new Bear("Bernard", "Male", 1200, "grizzly", 6)
+let Cathy = new Cougar("Cathy", "Female", 125, 9, 3)
+let Danny = new Donkey("Danny", "Male", 800, 15)
+let Elizabeth = new Elephant("Elizabeth", "Female", 5320, 7.5, 32)
+let Fanny = new Fox("Fanny", "Female", 60, "orange", 33)
+let Gerald = new Giraffe("Gerald", "Male", 1163, 12.4, 27)
+let Herbert = new HoneyBadger("Herbert", "Male", 36, 26, 13)
+let Indigo = new Iguana("Indigo", "Male", 6, 6, 18)
+let Jojo = new Jackrabbit("Jojo", "Male", 5, 4, 12)
+let Karen = new Kangaroo("Karen", "Female", 140, 60, 12.5)
+let Leroy = new Lion("Leroy", "Male", )
